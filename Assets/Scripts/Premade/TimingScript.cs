59dc476a73654ffefcada8e17ed6344900241360
Rasmus Jensen <rasje17@student.sdu.dk>
2020-05-01 14:31:11 +0200

    added the course
    tilfæjede pf 2 avleværings banen

---
+++ Assets/Scripts/Premade/TimingScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TimingScript : MonoBehaviour
+{
+    private string timerString = "Timer: ";
+    private float timer;
+    public TextMeshProUGUI text;
+
+    private bool timeUp;
+    void Start()
+    {
+        timer = 0;
+
+    }
+    void Update()
+    {
+        if (timeUp)
+        {
+            return;
+        }
+        timer += Time.deltaTime;
+        text.text = timerString + timer;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            timeUp = true;
+        }
+    }
+}
