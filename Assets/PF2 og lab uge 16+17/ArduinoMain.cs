9667acdffef6463d3dbd8367f587094954934051
pachr16 <32641694+pachr16@users.noreply.github.com>
2020-05-01 23:10:00 +0200

    Nu kan vi nå til halvvejs-linjen ---> Vi kan følge streger, dreje om obstacles og beslutte at vi nu er i wallAvoidance state

--- Assets/PF2 og lab uge 16+17/ArduinoMain.cs
+++ Assets/PF2 og lab uge 16+17/ArduinoMain.cs
@@ -1,10 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Threading;
+using UnityEngine;
 
-public class ArduinoMain : MonoBehaviour
-{
+public class ArduinoMain : MonoBehaviour {
     //On included/premade Arduino functions:
     //delay(timeInMilliseconds) : use "yield return delay(timeInMilliseconds)", to get similar functionality as delay() in arduino would give you.
 
@@ -41,131 +40,181 @@ public class ArduinoMain : MonoBehaviour
     private int forwardRight = 2, forwardLeft = 3;
     private int backRight = 4, backLeft = 5;
     private int distanceSensor = 6;
+    private string state = "LineFollower";
+    bool leftOverLine = false, rightOverLine = false;
 
-    IEnumerator setup()
-    {
-
+    IEnumerator setup () {
+        servo.write (90);
 
         //Example of delay:
-        Debug.Log("pre-delay log");
-        yield return delay(2000); //2 second delay
+        Debug.Log ("pre-delay log");
+        yield return delay (2000); //2 second delay
         //Your code ends here -----
 
         //following region ensures delay-functionality for setup() & loop(). Do not delete, must always be last thing in setup.
         #region PremadeSetup
-        yield return StartCoroutine(loop()); ;
+        yield return StartCoroutine (loop ());;
         #endregion PremadeSetup
     }
 
-    IEnumerator loop()
-    {
-        Debug.Log("Reading ldr left: " + analogRead(ldrLeft));
-        Debug.Log("Reading ldr right: " + analogRead(ldrRight));
+    IEnumerator loop () {
 
-        turnLeft();
+        if (state == "LineFollower") {
+            if (pulseIn (distanceSensor) < 800 && pulseIn (distanceSensor) != 0) {
+                stopAll ();
+                Debug.Log ("Vi måler en væg! Så langt væk: " + pulseIn (distanceSensor));
 
+                turn90Left ();
+                yield return delay (1200); // enough delay to turn ~89-91 degrees, with some uncertainty due to Unity
+                stopAll ();
+
+                servo.write(88);
+                yield return delay(2000);
+                if  (pulseIn(distanceSensor) < 1500) {
+                    Debug.Log("Changing state to WallAvoider!");
+                    state = "WallAvoider";
+                    yield return delay(1000);
+                }
+
+                if (state == "LineFollower") {
+                servo.write (180);
+                yield return delay (2000);
+                while (pulseIn (distanceSensor) == 0) {
+                    analogWrite (forwardRight, 50);
+                    analogWrite (forwardLeft, 50);
+                    yield return delay (100);
+                }
+                stopAll ();
+
+                while (pulseIn (distanceSensor) < 1000) {
+                    analogWrite (forwardLeft, 50);
+                    analogWrite (forwardRight, 20);
+                    yield return delay (10);
+
+                    if (analogRead (ldrRight) < 100 || analogRead (ldrLeft) < 100) {
+                        break;
+                    }
+                }
+                stopAll ();
+                }
+
+            } else {
+                if (analogRead (ldrLeft) > 900 && analogRead (ldrRight) > 900) {
+                    driveForwards ();
+                } else if (analogRead (ldrLeft) < 900 && analogRead (ldrRight) > 900) {
+                    turnLeft ();
+                } else if (analogRead (ldrLeft) > 900 && analogRead (ldrRight) < 900) {
+                    turnRight ();
+                }
+                yield return delay (10);
+            }
+
+        } else if (state == "TESTING") {
+
+        } else {
+            Debug.Log("We are now in WallAvoider state!");
+            yield return delay(10000);
+        }
 
         //Following region is implemented as to allow "yield return delay()" to function the same way as one would expect it to on Arduino.
         //It should always be at the end of the loop()-function, and shouldn't be edited.
         #region DoNotDelete
         //Wait for one frame
-        yield return new WaitForSeconds(0);
+        yield return new WaitForSeconds (0);
         //New loop():
-        yield return loop();
+        yield return loop ();
         #endregion DoNotDelete 
     }
 
-
-    void turnLeft() {
-        analogWrite(forwardLeft, 0);   // denne er ikke nødvendig hvis vi i stedet kalder stopAll før vi begynder at dreje
-        analogWrite(forwardRight, 50);
+    void turn90Left () {
+        analogWrite (backLeft, 50);
+        analogWrite (forwardRight, 50);
+        //yield return delay(1200);       // enough delay to turn ~89-91 degrees, with some uncertainty due to Unity
+        //stopAll();
     }
 
-    void turnRight() {
-        analogWrite(forwardRight, 0);   // denne er ikke nødvendig hvis vi i stedet kalder stopAll før vi begynder at dreje
-        analogWrite(forwardLeft, 50);
+    void turn90Right () {
+        analogWrite (backRight, 50);
+        analogWrite (forwardLeft, 50);
+        //yield return delay(1200);       // enough delay to turn ~89-91 degrees, with some uncertainty due to Unity
+        //stopAll();
     }
 
-    void driveForwards() {
-        analogWrite(forwardRight, 50);
-        analogWrite(forwardLeft, 50);
+    void turnLeft () {
+        servo.write (45);
+        analogWrite (forwardLeft, 0); // denne er ikke nødvendig hvis vi i stedet kalder stopAll før vi begynder at dreje
+        analogWrite (forwardRight, 50);
     }
 
-    void stopAll() {
-        analogWrite(forwardRight, 0);
-        analogWrite(forwardLeft, 0);
-        analogWrite(backRight, 0);
-        analogWrite(backLeft, 0);
+    void turnRight () {
+        servo.write (135);
+        analogWrite (forwardRight, 0); // denne er ikke nødvendig hvis vi i stedet kalder stopAll før vi begynder at dreje
+        analogWrite (forwardLeft, 50);
     }
 
+    void driveForwards () {
+        servo.write (90);
+        analogWrite (forwardRight, 50);
+        analogWrite (forwardLeft, 50);
+    }
 
-
-
+    void stopAll () {
+        analogWrite (forwardRight, 0);
+        analogWrite (forwardLeft, 0);
+        analogWrite (backRight, 0);
+        analogWrite (backLeft, 0);
+    }
 
     #region PremadeDefinitions
-    void Start()
-    {
-        servo = FindObjectOfType<Servo>();
-        if (servo == null)
-        {
-            Debug.Log("No servo found in the scene. Consider assigning it to 'ArduinoMain.cs' manually.");
+    void Start () {
+        servo = FindObjectOfType<Servo> ();
+        if (servo == null) {
+            Debug.Log ("No servo found in the scene. Consider assigning it to 'ArduinoMain.cs' manually.");
         }
         Time.fixedDeltaTime = 0.005f; //4x physics steps of what unity normally does - to improve sensor-performance.
-        StartCoroutine(setup());
-
+        StartCoroutine (setup ());
 
     }
 
-    IEnumerator delay(int _durationInMilliseconds)
-    {
-        float durationInSeconds = ((float)_durationInMilliseconds * 0.001f);
-        yield return new WaitForSeconds(durationInSeconds);
+    IEnumerator delay (int _durationInMilliseconds) {
+        float durationInSeconds = ((float) _durationInMilliseconds * 0.001f);
+        yield return new WaitForSeconds (durationInSeconds);
     }
 
-    public long map(long s, long a1, long a2, long b1, long b2)
-    {
+    public long map (long s, long a1, long a2, long b1, long b2) {
         return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
     }
 
-    public ulong millis()
-    {
-        return (ulong)(Time.timeSinceLevelLoad * 1000f);
+    public ulong millis () {
+        return (ulong) (Time.timeSinceLevelLoad * 1000f);
     }
 
-    public ulong abs(long x)
-    {
-        return (ulong)Mathf.Abs(x);
+    public ulong abs (long x) {
+        return (ulong) Mathf.Abs (x);
     }
 
-    public long constrain(long x, long a, long b)
-    {
+    public long constrain (long x, long a, long b) {
         return (x < a ? a : (x > b ? b : x));
     }
-
 
     #endregion PremadeDefinitions
 
     #region InterfacingWithBreadboard
-    public int analogRead(int pin)
-    {
-        return breadboard.analogRead(pin);
+    public int analogRead (int pin) {
+        return breadboard.analogRead (pin);
     }
-    public void analogWrite(int pin, int value)
-    {
-        breadboard.analogWrite(pin, value);
+    public void analogWrite (int pin, int value) {
+        breadboard.analogWrite (pin, value);
     }
-    public bool digitalRead(int pin)
-    {
-        return breadboard.digitalRead(pin);
+    public bool digitalRead (int pin) {
+        return breadboard.digitalRead (pin);
     }
-    public void digitalWrite(int pin, bool isHigh)
-    {
-        breadboard.digitalWrite(pin, isHigh);
+    public void digitalWrite (int pin, bool isHigh) {
+        breadboard.digitalWrite (pin, isHigh);
     }
 
-    public ulong pulseIn(int pin)
-    {
-        return breadboard.pulseIn(pin);
+    public ulong pulseIn (int pin) {
+        return breadboard.pulseIn (pin);
     }
     #endregion InterfacingWithBreadboard
 }
